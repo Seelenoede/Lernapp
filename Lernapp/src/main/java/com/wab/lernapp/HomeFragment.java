5a5f15ff50853c19a21e5864c1ad709f1720990f
Seelenoede <nerzhul1993@hotmail.de>
2015-04-23 16:26:02 +0200

    Add list to HomeFragement

--- Lernapp/src/main/java/com/wab/lernapp/HomeFragment.java
+++ Lernapp/src/main/java/com/wab/lernapp/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.wab.lernapp;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import java.io.File;
 
@@ -14,6 +18,8 @@ import java.io.File;
 public class HomeFragment extends Fragment {
 
     File[] allFiles;
+    private ListView mItemList;
+
     public HomeFragment() {
     }
 
@@ -25,11 +31,35 @@ public class HomeFragment extends Fragment {
 
         allFiles = FileHandler.getAllFiles();
 
-        for (File file : allFiles)
-        {
+        mItemList = (ListView) rootView.findViewById(R.id.fileList);
 
+        ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[allFiles.length - 1];
+
+        int count = 0;
+
+        for(File file : allFiles)
+        {
+            //drawerItem[count] = new ObjectDrawerItem(icon, "Name");
+            count++;
         }
 
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(rootView.getContext(), R.layout.listview_item_row, drawerItem);
+        mItemList.setAdapter(adapter);
+        mItemList.setOnItemClickListener(new ListFileItemClickListener());
+
         return rootView;
+    }
+
+    private class ListFileItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    private void selectItem(int position) {
+
     }
 }
