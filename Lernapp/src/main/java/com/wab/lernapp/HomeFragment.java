d4146e74af9a228e8687c9d1d1e0859c844f7919
Seelenoede <nerzhul1993@hotmail.de>
2015-04-24 12:41:40 +0200

    Kleine Anpassung
    setBackgroundColor in HomeFragment entfernt, um besser mergen zu können

--- Lernapp/src/main/java/com/wab/lernapp/HomeFragment.java
+++ Lernapp/src/main/java/com/wab/lernapp/HomeFragment.java
@@ -30,7 +30,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        rootView.setBackgroundColor(((MainActivity)getActivity()).currentColor);
+        //rootView.setBackgroundColor(((MainActivity)getActivity()).currentColor);
 
         fileHandler = new FileHandler();
 
